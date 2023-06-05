2c402300e6490e59224cc914870bbc04b051865d
Justin Kenealy <justinmkenealy@gmail.com>
2023-06-05 17:18:26 +1000

    updates landing page styling

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,15 +1,16 @@
 import { Button, Heading, Text } from "@chakra-ui/react";
 import React from "react";
 import { Link, Navigate } from "react-router-dom";
+import "./Landing.css"
 
 const LandingPage = () => {
 
   return (
-    <div>
+    <div id="homepage">
       <Heading>Welcome to Harry's House Pet Store</Heading>
-      <Text fontSize='2xl'>
+      <Text margin="40px 0" fontSize='2xl'>
         We offer a wide range of pet supplies and accessories for your best
-        friends no matter their shape or size.
+        friends no matter their shape or size. Head to our store to check out our products!
       </Text>
       <Link to="/store"><Button>Shop Now</Button></Link>
     </div>
