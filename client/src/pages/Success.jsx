97d8e1b88afe15f65213ed01838ffa8cc7257a47
Justin Kenealy <justinmkenealy@gmail.com>
2023-06-06 14:21:37 +1000

    updates success page

--- client/src/pages/Success.jsx
+++ client/src/pages/Success.jsx
@@ -1,6 +1,17 @@
+import { Button, Center, Image, Text } from "@chakra-ui/react"
+import { Link } from "react-router-dom"
+
 const Success = () => {
     return (
-       <h1>Thank you for your purchase!</h1> 
+        <div style={{margin: '50px auto', textAlign: 'center'}}>
+        <Text>Thank you for your order!</Text>
+        <Image
+          height="200px"
+          src="http://localhost:3000/assets/logo/corgi_logo2.png"
+          margin="15px"
+        />
+        <Link to="/store"><Button colorScheme="blue">Return to Store</Button></Link>
+      </div>
     )
 }
 
