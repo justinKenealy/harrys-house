322e21e995b59c49caa26d9760cb62ab196bd44d
Justin Kenealy <justinmkenealy@gmail.com>
2023-06-08 19:27:48 +1000

    adds login failed message to front end

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,8 +1,10 @@
-import { Button, Input } from "@chakra-ui/react";
+import { Button, Input, Text } from "@chakra-ui/react";
+import { useState } from "react";
 import { useAuth } from "../contexts/AuthProvider";
 
 const LoginForm = () => {
   const { login } = useAuth();
+  const [loginFailed, setLoginFailed] = useState(false)
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -10,13 +12,14 @@ const LoginForm = () => {
     try {
       await login(fields);
     } catch (err) {
-      // Show error message
-      console.log(err);
+      setLoginFailed(true)
+      // console.log(err);
     }
   };
 
   return (
     <form onSubmit={handleSubmit}>
+      {loginFailed && <Text>Username or password is incorrect. Try again.</Text>}
       <Input backgroundColor="white" margin="5px 0" type="text" name="username" placeholder="username" />
       <Input backgroundColor="white" margin="5px 0" type="password" name="password" placeholder="password" />
       <Button colorScheme="blue" margin="5px 0" type="submit" value="Login">Login</Button>
