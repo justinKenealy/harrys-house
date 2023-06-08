a4e47fb25725396de67f203f94db747ae0c5acca
Justin Kenealy <justinmkenealy@gmail.com>
2023-06-08 18:45:32 +1000

    adds model and controller for update user password

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const express = require("express");
-const { getAllUsers, registerUser } = require("../models/user");
-const { generateHash } = require("../utils/password-verification");
+const { getAllUsers, getUserById, registerUser, updateUserPasswordById } = require("../models/user");
+const { generateHash, comparePassword } = require("../utils/password-verification");
 
 const router = express.Router();
 
@@ -60,5 +60,45 @@ router.get("/", async (req, res, next) => {
     next(err);
   }
 });
+
+//update password
+router.put('/users/:id', async (req, res, next) => {
+  try {
+    const id = Number(req.params.id)
+      const { passwordOld, passwordNew } = req.body
+      const user = await getUserById(id)
+      if (comparePassword(passwordOld, user.password_hash)) {
+        if (passwordNew.length < 8) {
+          const customError = new Error('The password is too short. It must be 8 characters long.')
+          customError.status = 400
+          return next(customError)
+        }
+        const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/
+        if (
+          !/\d/.test(passwordNew) ||
+          !/[A-Z]/.test(passwordNew) ||
+          !/[a-z]/.test(passwordNew) ||
+          !specialChars.test(passwordNew)
+        ) {
+          const customError = new Error(
+            'The password must contain a combination of uppercase letters, lowercase letters, numbers and symbols.'
+          )
+          customError.status = 400
+          return next(customError)
+        }
+        const newPasswordHash = generateHash(passwordNew)
+        const userRow = await updateUserPasswordById(id, newPasswordHash)
+        if (userRow === 0) {
+          return res.sendStatus(404)
+        }
+        return res.status(200).json({
+          message: 'Updated successfully.',
+        })
+      }
+      
+    } catch (err) {
+    next(err)
+  }
+})
 
 module.exports = router;
--- server/models/user.js
+++ server/models/user.js
@@ -2,12 +2,24 @@ const db = require("../db/index");
 
 const registerUser = (username, email, password_hash) => {
   const sql =
-    'INSERT INTO users (username, email, password_hash) VALUES ($1, $2, $3);'
-    return db.query(sql, [username, email, password_hash]).then((res) => res.rows[0])
-}
+    "INSERT INTO users (username, email, password_hash) VALUES ($1, $2, $3);";
+  return db
+    .query(sql, [username, email, password_hash])
+    .then((res) => res.rows[0]);
+};
 
 const getAllUsers = () => {
-    return db.query('SELECT * FROM users;').then((res) => res.rows)
-  }
+  return db.query("SELECT * FROM users;").then((res) => res.rows);
+};
 
-module.exports = { registerUser, getAllUsers }
+const getUserById = (id) => {
+  const sql = "SELECT * FROM users WHERE id=$1"
+  return db.query(sql, [id]).then((res) => res.rows);
+};
+
+const updateUserPasswordById = (id, password_hash) => {
+  const sql = "UPDATE users SET password_hash=$1 WHERE id=$2";
+  return db.query(sql, [password_hash, id]).then((res) => res.rowCount);
+};
+
+module.exports = { registerUser, getUserById, getAllUsers, updateUserPasswordById };
